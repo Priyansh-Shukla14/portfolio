f36c74567c219aef5800274705ac2fec74d65963
Priyansh Shukla <priyanshshukla715i@gmail.com>
2026-04-07 22:12:37 +0530

    other changes done

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 const port = parseInt(process.env.PORT || "5173");
-const basePath = "/portfolio/";
+const basePath = process.env.VERCEL ? "/" : "/portfolio/";
 
 export default defineConfig({
   base: basePath,
